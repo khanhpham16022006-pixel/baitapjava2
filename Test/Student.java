4b8788f705fb514cf5deedd482bf9985af0abc06
khanhpham16022006-pixel <khanhpham16022006@gmail.com>
2025-09-25 03:35:30 +0700

    Update Student.java

--- Test/Student.java
+++ Test/Student.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Test;
 
 import java.util.Scanner;
@@ -71,4 +67,5 @@ public class Student {
     public double getAverageScore() {
         return this.averageScore;
     }
+
 }
